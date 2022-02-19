0ae7ab7f7089ff8712cea8928ffa0a90fb3d60f3
Gabriel Donato <gabrielcdonato@gmail.com>
2022-02-18 21:58:44 -0300

    Feat:(APP) app widgets routes

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 
 import 'modules/home/pages/home_page.dart';
+import 'modules/splash/pages/splash_page.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Flutter Demo',
-      home: HomePage(),
+      routes: {
+        '/home_page': (route) => const HomePage(),
+        '/splash_page': (route) => const SplashPage(),
+      },
     );
   }
 }
