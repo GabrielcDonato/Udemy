55e96abb4dc74bf4ea6fb5248cb1aea86169dc65
Gabriel Donato <gabrielcdonato@gmail.com>
2022-02-18 22:27:34 -0300

    Feat:(SPLASH) finish the splash page

--- lib/modules/splash/pages/splash_page.dart
+++ lib/modules/splash/pages/splash_page.dart
@@ -1,10 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:udemy/core/core.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
 
   @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 5)).then(
+      (_) => Navigator.of(context).pushReplacementNamed('/home_page'),
+    );
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        color: const Color(0xFF000000),
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 240,
+            ),
+            SizedBox(
+              height: 200,
+              width: 200,
+              child: Image.asset(AppImages.splash),
+            ),
+            const Spacer(),
+            SizedBox(
+              height: 25,
+              width: 25,
+              child: CircularProgressIndicator(
+                color: Colors.deepPurpleAccent.shade100,
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
