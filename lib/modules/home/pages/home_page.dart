41e55e0641dfd9924b372cf89b15aa14caac0fae
Gabriel Donato <gabrielcdonato@gmail.com>
2022-02-18 23:47:58 -0300

    Feat:(HOME) add concept home

--- lib/modules/home/pages/home_page.dart
+++ lib/modules/home/pages/home_page.dart
@@ -5,6 +5,45 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF000000),
+        actions: [
+          InkWell(
+            onTap: () => Navigator.of(context).pushNamed("/cart_page"),
+            child: const Icon(
+              Icons.shopping_cart_outlined,
+              color: Color(0xFFffffff),
+              size: 35,
+            ),
+          )
+        ],
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        color: const Color(0xFF000000),
+        child: Column(
+          children: const [
+            Text(
+              'data',
+              style: TextStyle(color: Colors.white),
+            ),
+            Text(
+              'data',
+              style: TextStyle(color: Colors.white),
+            ),
+            Text(
+              'data',
+              style: TextStyle(color: Colors.white),
+            ),
+            Text(
+              'data',
+              style: TextStyle(color: Colors.white),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
