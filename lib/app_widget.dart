717b3b4dc42458145df698857eaeac54b6d03018
Gabriel Donato <gabrielcdonato@gmail.com>
2022-02-18 23:49:12 -0300

    Feat:(ROUTES) organize routes and add cart route page

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 
+//Splash Page:
 import 'modules/splash/splash.dart';
+
+//Home Page:
 import 'modules/home/home.dart';
+
+//Cart Page:
+import 'modules/cart/cart.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({Key? key}) : super(key: key);
@@ -15,6 +21,7 @@ class AppWidget extends StatelessWidget {
       routes: {
         '/splash_page': (route) => const SplashPage(),
         '/home_page': (route) => const HomePage(),
+        '/cart_page': (route) => const CartPage(),
       },
     );
   }
